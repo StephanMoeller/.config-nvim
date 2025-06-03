f958b6ec90250bfb766ede122cad882e7b41e68f
Stephan Moeller <stephanryer@hotmail.com>
2025-06-03 08:35:02 +0200

    transparent background color in nvim

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,6 +2,16 @@ return {
   "catppuccin/nvim", name = "catppuccin", priority = 1000,
   config = function()
     vim.cmd.colorscheme "catppuccin"
+  -- ensure background color is controlled by terminal (to let tmux change it depending on focus)
+  vim.cmd [[
+    highlight Normal       guibg=NONE ctermbg=NONE
+    highlight NormalNC     guibg=NONE ctermbg=NONE
+    highlight VertSplit    guibg=NONE ctermbg=NONE
+    highlight SignColumn   guibg=NONE ctermbg=NONE
+    highlight LineNr       guibg=NONE ctermbg=NONE
+    highlight EndOfBuffer  guibg=NONE ctermbg=NONE
+  ]]
+
   end
 
 }
