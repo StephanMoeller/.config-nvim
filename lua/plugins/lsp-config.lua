0c65e09ea2bca7d3e44ecc31b39bba815d90c48c
Stephan Moeller <stephanryer@hotmail.com>
2025-06-09 11:08:19 +0200

    setup the lsp, autoformatting and error showing

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,14 +19,33 @@ return {
       lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.zls.setup({})
+
+
+
       vim.keymap.set('n', '<leader>i', vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<c-e>", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<m-c-e>", vim.lsp.buf.references, {})
       vim.keymap.set({ "n", "v" }, "<leader>s", vim.lsp.buf.code_action, {})
-      
+
       vim.keymap.set("n", "<s-Up>", vim.diagnostic.goto_prev, {})
       vim.keymap.set("n", "<s-Down>", vim.diagnostic.goto_next, {})
 
+
+      -- show errors when done writing (in opposition to update_in_instert which will trigger on every keystroke)
+      vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
+        callback = function()
+          vim.diagnostic.show(nil, 0)
+        end,
+      }) 
+
+      -- Auto format on save
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+
+      -- Ensure inline errors
       vim.diagnostic.config({
         virtual_text = true, -- inline errors
         signs = true,
