95a90245d8888f9f0aa2e167607e27f39356a60f
Stephan Moeller <stephanryer@hotmail.com>
2025-06-09 10:57:58 +0200

    more lsp config setup

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,7 +1,7 @@
 return {
   {
     "williamboman/mason.nvim",
-    config = function() 
+    config = function()
       require("mason").setup()
     end
   },
@@ -19,10 +19,21 @@ return {
       lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.zls.setup({})
-      vim.keymap.set('n', '<leader>i', vim.lsp.buf.hover, {}) 
+      vim.keymap.set('n', '<leader>i', vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<c-e>", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<m-c-e>", vim.lsp.buf.references, {})
       vim.keymap.set({ "n", "v" }, "<leader>s", vim.lsp.buf.code_action, {})
+      
+      vim.keymap.set("n", "<s-Up>", vim.diagnostic.goto_prev, {})
+      vim.keymap.set("n", "<s-Down>", vim.diagnostic.goto_next, {})
+
+      vim.diagnostic.config({
+        virtual_text = true, -- inline errors
+        signs = true,
+        underline = true,
+        update_in_insert = false, -- run while typing (annoying to look at)
+        severity_sort = true,
+      })
     end
   },
 }
