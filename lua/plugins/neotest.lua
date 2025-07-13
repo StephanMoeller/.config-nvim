5c77605f76539cf252fac9c97817189f771ded91
Stephan Moeller <stephanryer@hotmail.com>
2025-07-13 22:47:08 +0200

    making neotest summary automatically show when running tests

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -20,13 +20,25 @@ return {
         }
       })
       local opts = { noremap = true, silent = true }
-      vim.keymap.set("n", "<leader>tt", function() neotest.run.run() end, opts)                     -- Run nearest test
-      vim.keymap.set("n", "<leader>tf", function() neotest.run.run(vim.fn.expand("%")) end, opts)   -- Run file
+      -- Run nearest test
+      vim.keymap.set("n", "<leader>tt", function()
+        neotest.run.run()
+        neotest.summary.open()
+      end, opts)
+      -- Run file
+      vim.keymap.set("n", "<leader>tf", function()
+        neotest.run.run(vim.fn.expand("%"))
+        neotest.summary.open()
+      end, opts)
+      -- Run all
+      vim.keymap.set("n", "<leader>ta", function()
+        neotest.run.run({ suite = true })
+        neotest.summary.open()
+      end, opts)
       vim.keymap.set("n", "<leader>td", function() neotest.run.run({ strategy = "dap" }) end, opts) -- Debug test
       vim.keymap.set("n", "<leader>ts", function() neotest.summary.toggle() end, opts)              -- Toggle summary
       vim.keymap.set("n", "<leader>to", function() neotest.output.open({ enter = true }) end, opts) -- Open output
       vim.keymap.set("n", "<leader>tO", function() neotest.output_panel.toggle() end, opts)         -- Toggle output panel
-      vim.keymap.set("n", "<leader>ta", function() neotest.run.run({ suite = true }) end, opts)     -- Run all
     end
   },
   {
