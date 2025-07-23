3322eab2879fed9256d3eeaa92c97faf37c1f63d
Stephan Moeller <stephanryer@hotmail.com>
2025-07-23 22:41:30 +0200

    showing dot files in neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,40 @@
-return   {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      -- Configure neo-tree
-      vim.keymap.set('n', '<M-e>', ':Neotree filesystem reveal left<CR>')
-    end
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opst = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        visible = true,
+        never_show = {},
+      }
+    }
+
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- add extension names you want to explicitly exclude
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      },
+    })
+    -- Configure neo-tree
+    vim.keymap.set('n', '<M-e>', ':Neotree filesystem reveal left<CR>')
+  end
 }
