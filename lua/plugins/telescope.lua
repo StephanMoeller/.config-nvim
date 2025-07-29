be0f2be461f5158cae11452a3315d81336d4e713
Stephan Moeller <stephanryer@hotmail.com>
2025-07-29 22:50:22 +0200

    opening new files in main area always

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,33 @@ return {
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+      -- Save the main_win at startup time
+      local main_win = nil
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          main_win = vim.api.nvim_get_current_win()
+        end
+      })
+
+      -- Opret en funktion der hopper til main window og åbner Telescope
+      local function open_files_in_main()
+        if main_win and vim.api.nvim_win_is_valid(main_win) then
+          vim.api.nvim_set_current_win(main_win)
+        end
+        require('telescope.builtin').find_files()
+      end
+      local function live_grep_in_main()
+        if main_win and vim.api.nvim_win_is_valid(main_win) then
+          vim.api.nvim_set_current_win(main_win)
+        end
+        require('telescope.builtin').live_grep()
+      end
       -- Configure telescope
-      local telescope_config = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>fn', telescope_config.find_files, {})
-      vim.keymap.set('n', '<leader>fg', telescope_config.live_grep, {}) -- requires ripgrep to be install on order to work
+      --local telescope_config = require("telescope.builtin")
+      --vim.keymap.set('n', '<leader>fn', telescope_config.find_files, {})
+
+      vim.keymap.set('n', '<leader>fn', open_files_in_main, {})
+      vim.keymap.set('n', '<leader>fg', live_grep_in_main, {}) -- requires ripgrep to be install on order to work
     end
   },
   {
